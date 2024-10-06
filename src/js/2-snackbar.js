d09d2ab0789a7fc73553d8ed70068175549b18ab
1ster14ka <danyazybastik@gmail.com>
2024-10-06 17:41:11 +0300

    done task

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,56 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const ms = document.querySelector('.form > label input');
+
+const fieldset = document.querySelector('fieldset');
+
+const userData = {
+  value: '',
+  delay: '',
+};
+
+ms.addEventListener('input', event => {
+  userData.delay = ms.value;
+});
+
+fieldset.addEventListener('click', event => {
+  if (event.target.type === 'radio') {
+    userData.value = event.target.value;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  form.reset();
+  if (userData.value && userData.delay) {
+    fetchUserServer(userData).then().catch();
+  }
+});
+
+const fetchUserServer = ({ value, delay }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 'fulfilled') {
+        resolve(
+          `${iziToast.show({
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+            color: 'green',
+          })}`
+        );
+      } else {
+        reject(
+          `${iziToast.show({
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topRight',
+            color: 'red',
+          })}`
+        );
+      }
+    }, delay);
+  });
+};
